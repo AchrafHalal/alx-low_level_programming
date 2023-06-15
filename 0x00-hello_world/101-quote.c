898f33f9a83485b2413f4af4c2c58aa2a57ea4d7
AchrafHL1 <achraf.halal.ax@gmail.com>
2023-06-15 16:41:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,14 @@
-/*
- *  * File: 101-quote.c
- *   * Auth: Brennan D Baraban
- *    */
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
-
-			return (1);
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
