23fab00e11ec71ce5009f978328ea806ed7eeab8
AchrafHL1 <achraf.halal.ax@gmail.com>
2023-06-20 17:54:37 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *  * print_alphabet - Entry point
+ *   *
+ *    * Description: function print alphabet in lowercase
+ *     *
+ *      */
+void print_alphabet(void)
+{
+		char letter;
+
+			for (letter = 'a' ; letter <= 'z' ; letter++)
+					{
+								_putchar(letter);
+									}
+				_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _islower - function that checks for lowercase character
+ *
+ * @c: Check the input
+ *
+ * Return: if lower Always 1.
+ * if upper Always 0.
+ */
+
+int _islower(int c)
+{
+			return (c >= 97 && c <= 122);
+}
